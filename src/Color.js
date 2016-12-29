2198c4d5df2446bb9cf3531d0d4e14cf0d2b0816
Luuk <info@luuk.site>
2016-12-29 15:12:40 +0100

    Refactor: OOP

--- src/Color.js
+++ src/Color.js
@@ -1,34 +1,40 @@
 (function(root) {
-  var _callback = null;
-  var _data = null;
-  var _img = null;
-  var _method = null;
-  var _url = null;
+  var Color = function(item) {
+    this._callback = null;
+    this._data = null;
+    this._img = null;
+    this._method = null;
+    this._url = null;
+
+    if (typeof item === 'object' && item.src) {
+      this._url = item.src;
+    } else if (typeof item === 'string') {
+      this._url = item;
+    }
+  };
 
   /**
    * Internal functions
    */
 
-  function _createImage() {
+  Color.prototype._createImage = function() {
     // Short-circuit if data is already present from previous run
-    if (_data) {
-      _method();
+    if (this._data) {
+      this._method();
 
       return;
     }
 
-    var img = document.createElement('img');
+    this._img = document.createElement('img');
+    this._img.crossOrigin = 'Anonymous';
+    this._img.src = this._url;
 
-    img.crossOrigin = 'Anonymous';
-    img.src = _url;
+    this._img.addEventListener('load', function() {
+      this._createCanvas();
+    }.bind(this));
+  };
 
-    img.addEventListener('load', function() {
-      _img = img;
-      _createCanvas();
-    });
-  }
-
-  function _createCanvas() {
+  Color.prototype._createCanvas = function() {
     var canvas = document.createElement('canvas');
 
     if (typeof canvas.getContext === 'undefined') {
@@ -37,30 +43,41 @@
 
     var context = canvas.getContext('2d');
 
-    canvas.height = _img.height;
+    canvas.height = this._img.height;
     canvas.style.display = 'none';
-    canvas.width = _img.width;
-    context.drawImage(_img, 0, 0);
+    canvas.width = this._img.width;
+    context.drawImage(this._img, 0, 0);
 
     document.body.appendChild(canvas);
 
-    var info = context.getImageData(0, 0, _img.width, _img.height);
-    _data = info.data;
+    var info = context.getImageData(0, 0, this._img.width, this._img.height);
+    this._data = info.data;
 
     document.body.removeChild(canvas);
 
-    _method();
-  }
+    this._method();
+  };
 
-  function _extract() {
+  Color.prototype._average = function() {
+    var colors = [];
+    var channels = this._extract();
+
+    for (var key in channels) {
+      colors.push(this._format(channels[key].total / channels[key].amount));
+    }
+
+    this._callback('rgb(' + colors.join(', ') + ')');
+  };
+
+  Color.prototype._extract = function() {
     var channels = {
       r: {amount: 0, total: 0},
       g: {amount: 0, total: 0},
       b: {amount: 0, total: 0},
     };
 
-    for (var i = 0; i < (_img.width * _img.height); i += 4) {
-      if (_data[i + 3] < (255 / 2)) {
+    for (var i = 0; i < (this._img.width * this._img.height); i += 4) {
+      if (this._data[i + 3] < (255 / 2)) {
         continue;
       }
 
@@ -68,7 +85,7 @@
 
       for (var key in channels) {
         channels[key].amount++;
-        channels[key].total += _data[iterator];
+        channels[key].total += this._data[iterator];
 
         iterator++;
       }
@@ -77,18 +94,7 @@
     return channels;
   }
 
-  function _average() {
-    var colors = [];
-    var channels = _extract();
-
-    for (var key in channels) {
-      colors.push(_format(channels[key].total / channels[key].amount));
-    }
-
-    _callback('rgb(' + colors.join(', ') + ')');
-  };
-
-  function _format(value) {
+  Color.prototype._format = function(value) {
     return Math.round(value);
   }
 
@@ -96,19 +102,14 @@
    * External API
    */
 
-  var Color = function(item) {
-    if (typeof item === 'object' && item.src) {
-      _url = item.src;
-    } else if (typeof item === 'string') {
-      _url = item;
-    }
-  };
-
   Color.prototype.average = function(callback) {
-    _method = _average;
-    _callback = callback;
+    if (typeof callback !== 'function') {
+      throw new Error('Callback is not provided.');
+    }
 
-    _createImage();
+    this._method = this._average;
+    this._callback = callback;
+    this._createImage();
   }
 
   /**
