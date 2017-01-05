7e63274c85a6f450d8a4be2c93923c851cf391ab
Luuk <info@luuk.site>
2017-01-05 18:39:07 +0100

    Test: call without arguments

--- test/Color.test.js
+++ test/Color.test.js
@@ -1,10 +1,20 @@
 import browserEnv from 'browser-env';
 import Color from '../dist/color';
+import imageData from '../mock/image-data';
 import test from 'ava';
 
 browserEnv();
 
-const mock = new Color('../img/flag.jpg');
+const mock = new Color('fake');
+mock._data = imageData;
+
+test('Call without arguments', t => {
+  const error = t.throws(() => {
+    new Color();
+  });
+
+  t.is(error.name, 'TypeError');
+});
 
 test('RGB to HEX', t => {
   t.is(mock._rgbToHex(255, 150, 50), '#ff9632');
