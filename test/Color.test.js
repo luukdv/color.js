c5997627ac244b2af9ae864d34c8bf056c2a1dfa
Luuk <info@luuk.site>
2017-01-05 18:07:55 +0100

    Test rgb -> hex

--- test/Color.test.js
+++ test/Color.test.js
@@ -5,3 +5,9 @@ import test from 'ava';
 browserEnv();
 
 const mock = new Color('../img/flag.jpg');
+
+test('RGB to HEX', t => {
+  t.is(mock._rgbToHex(255, 150, 50), '#ff9632');
+  t.is(mock._rgbToHex(136, 127, 118), '#887f76');
+  t.is(mock._rgbToHex(94, 0, 0), '#5e0000');
+});
