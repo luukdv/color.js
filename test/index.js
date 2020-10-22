eb886f3a46a6d2057f7da14e4e92ac02187328d6
Luuk de Vlieger <info@luuk.site>
2020-10-22 13:35:22 +0200

    :white_check_mark: Add tests

--- test/index.js
+++ test/index.js
@@ -5,7 +5,17 @@ it('Calculates the average color', () => {
     .then((data) => expect(data).to.deep.equal([182, 180, 208]))
 })
 
+it('Calculates the average color with a strict sample size', () => {
+  cy.wrap(average('http://localhost:9000/sand.jpg', { sample: 1 }))
+    .then((data) => expect(data).to.deep.equal([182, 180, 208]))
+})
+
 it('Calculates the average color with a less strict sample size', () => {
   cy.wrap(average('http://localhost:9000/sand.jpg', { sample: 50 }))
     .then((data) => expect(data).to.deep.equal([182, 180, 207]))
+})
+
+it('Returns the color in hexadecimal format', () => {
+  cy.wrap(average('http://localhost:9000/sand.jpg', { format: 'hex' }))
+    .then((data) => expect(data).to.deep.equal('#b6b4d0'))
 })
