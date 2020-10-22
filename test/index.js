8fad795008baa9669124c1032fe12d9d4c8ead23
Luuk de Vlieger <info@luuk.site>
2020-10-22 16:54:52 +0200

    :white_check_mark: Add test

--- test/index.js
+++ test/index.js
@@ -2,7 +2,12 @@ import { average, prominent } from '../src'
 
 it('Calculates the average color', () => {
   cy.wrap(average('http://localhost:9000/sand.jpg'))
-    .then((data) => expect(data).to.deep.equal([182, 180, 208]))
+    .then((data) => expect(data).to.deep.equal([183, 180, 208]))
+})
+
+it('Calculates the prominent color', () => {
+  cy.wrap(prominent('http://localhost:9000/sand.jpg', { amount: 1 }))
+    .then((data) => expect(data).to.deep.equal([140, 140, 180]))
 })
 
 it('Calculates the average color with a strict sample size', () => {
@@ -17,5 +22,5 @@ it('Calculates the average color with a less strict sample size', () => {
 
 it('Returns the color in hexadecimal format', () => {
   cy.wrap(average('http://localhost:9000/sand.jpg', { format: 'hex' }))
-    .then((data) => expect(data).to.deep.equal('#b6b4d0'))
+    .then((data) => expect(data).to.deep.equal('#b7b4d0'))
 })
