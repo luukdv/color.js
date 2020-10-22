54a92b4fef58bee47f5239393a21228d22e0af36
Luuk de Vlieger <info@luuk.site>
2020-10-22 16:54:42 +0200

    :sparkles: Get prominent colors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 const getSrc = (item) => typeof item === 'object' && item.src ? item.src : item
 
 const getArgs = ({
-  amount = 10,
+  amount = 5,
   format = 'array',
   group = 20,
   sample = 10,
 } = {}) => ({ amount, format, group, sample })
 
-const rgbToHex = (rgb) => '#' + rgb.map((value) => {
-  const hex = value.toString(16)
+const rgbToHex = (rgb) => '#' + rgb.map((val) => {
+  const hex = val.toString(16)
 
   return hex.length === 1 ? '0' + hex : hex
 }).join('')
 
-const format = (data, type) => type === 'hex' ? rgbToHex(data) : data
+const format = (data, args) => {
+  const list = data.map((val) => {
+    const rgb = Array.isArray(val) ? val : val.split(',').map(Number)
+
+    return args.format === 'hex' ? rgbToHex(rgb) : rgb
+  })
+
+  return args.amount === 1 || list.length === 1 ? list[0] : list
+}
 
 const getImageData = (src) => new Promise((resolve, reject) => {
   const canvas = document.createElement('canvas')
@@ -34,6 +42,8 @@ const getImageData = (src) => new Promise((resolve, reject) => {
   img.src = src
 })
 
+const group = (number, amount) => Math.round(number / amount) * amount
+
 const getAverage = (data, args) => {
   const interval = 4 * args.sample
   const amount = data.length / interval
@@ -45,14 +55,34 @@ const getAverage = (data, args) => {
     rgb.b += data[i + 2]
   }
 
-  return format([
+  return format([[
     Math.round(rgb.r / amount),
     Math.round(rgb.g / amount),
     Math.round(rgb.b / amount)
-  ], args.format)
+  ]], args)
 }
 
 const getProminent = (data, args) => {
+  const interval = 4 * args.sample
+  const colors = {}
+
+  for (let i = 0; i < data.length; i += interval) {
+    const rgb = [
+      group(data[i], args.group),
+      group(data[i + 1], args.group),
+      group(data[i + 2], args.group),
+    ].join()
+
+    colors[rgb] = colors[rgb] ? colors[rgb] + 1 : 1
+  }
+
+  return format(
+    Object.entries(colors)
+      .sort(([keyA, valA], [keyB, valB]) => valA > valB ? -1 : 1)
+      .slice(0, args.amount)
+      .map(([rgb]) => rgb),
+    args
+  )
 }
 
 const process = (item, args, fn) => new Promise((resolve, reject) =>
