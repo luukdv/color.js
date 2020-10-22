ae835ab450eb3ab056f5746ebfb447c7181e4a94
Luuk de Vlieger <info@luuk.site>
2020-10-22 18:05:21 +0200

    :white_check_mark: Add test

--- test/index.js
+++ test/index.js
@@ -1,26 +1,35 @@
 import { average, prominent } from '../src'
 
-it('Calculates the average color', () => {
+it('Extracts the average color', () => {
   cy.wrap(average('http://localhost:9000/sand.jpg'))
     .then((data) => expect(data).to.deep.equal([183, 180, 208]))
 })
 
-it('Calculates the prominent color', () => {
+it('Extracts the prominent color', () => {
   cy.wrap(prominent('http://localhost:9000/sand.jpg', { amount: 1 }))
     .then((data) => expect(data).to.deep.equal([140, 140, 180]))
 })
 
-it('Calculates the average color with a strict sample size', () => {
+it('Extracts prominent colors', () => {
+  cy.wrap(prominent('http://localhost:9000/faces.jpg', { amount: 3, group: 28 }))
+    .then((data) => expect(data).to.deep.equal([
+      [0, 0, 0], // Black
+      [224, 0, 0], // Red
+      [0, 252, 252], // Blue
+    ]))
+})
+
+it('Extracts the average color with a strict sample size', () => {
   cy.wrap(average('http://localhost:9000/sand.jpg', { sample: 1 }))
     .then((data) => expect(data).to.deep.equal([182, 180, 208]))
 })
 
-it('Calculates the average color with a less strict sample size', () => {
+it('Extracts the average color with a less strict sample size', () => {
   cy.wrap(average('http://localhost:9000/sand.jpg', { sample: 50 }))
     .then((data) => expect(data).to.deep.equal([182, 180, 207]))
 })
 
 it('Returns the color in hexadecimal format', () => {
   cy.wrap(average('http://localhost:9000/sand.jpg', { format: 'hex' }))
-    .then((data) => expect(data).to.deep.equal('#b7b4d0'))
+    .then((data) => expect(data).to.equal('#b7b4d0'))
 })
